04e64e3ca90a6b2b2174734b46d04fd5b3d436d6
jordan8659 <andrew.m.jordan@outlook.com>
2020-09-04 23:47:56 -0500

    Creation of virtual environment and first module files.

---
+++ constant.py
@@ -0,0 +1,6 @@
+"""
+Constant variables for the purpose of development.
+"""
+
+OUTGOING_NUMBER = 2059646247
+RECEIVING_NUMBER = 7088281242
---
+++ phone_blast.py
@@ -0,0 +1,5 @@
+"""
+Main program for using Twilio to disseminate urgent information via a phone blast
+"""
+
+import constant
